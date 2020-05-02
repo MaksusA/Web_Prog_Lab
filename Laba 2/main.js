7eea275c151ce79e95686a49c517ebcaf8947bca
MaksusA <sinichuk2012@mail.ru>
2020-05-02 19:51:37 +0300

    2-3 labs

---
+++ Laba 2/main.js
@@ -0,0 +1,13 @@
+function pov (){
+	let out = document.getElementById('out');
+	var ch = '';
+	var cf = ' ';
+		for (var i=1 ; i <= num.value; i++) {
+		  alert(i);
+		  	ch = ch+i;
+		  out.innerHTML = ch;
+		}
+}
+
+
+
